ec071730152b7cad8dfa40e41782d1af70f1d628
izlyr <fuzzkillofficial@gmail.com>
2017-01-30 09:52:57 +0300

    [Chore]: Decode username to utf-8 and adhere to PEP 8

--- app/items/bucketlist_items.py
+++ app/items/bucketlist_items.py
@@ -65,69 +65,84 @@ DELETE: Removes the item from the bucketlist
                             "date_created": item.date_created,
                             "date_modified": item.date_modified,
                             "bucketlist": item.bucketlist.name,
-                            "owner": item.bucketlist.creator.name
+                            "owner": item.bucketlist.creator.name.decode('utf-8')
                         }
                         item_list.append(result)
-                    next_page = 'None'
-                    previous_page = 'None'
-                    if bucketlist_items.has_next:
-                        next_page = '{}api/v1/bucketlists/{}/items?limit={}&page={}'.format(
-                            str(request.url_root),
-                            str(bucketlist_id),
-                            str(limit),
-                            str(page_no + 1))
-                    if bucketlist_items.has_prev:
-                        previous_page = '{}api/v1/bucketlists/{}/items?limit={}&page={}'.format(
-                            str(request.url_root),
-                            str(bucketlist_id),
-                            str(limit),
-                            str(page_no - 1))
-                    return jsonify({'bucketlist items': item_list,
-                                    'total pages': bucketlist_items.pages,
-                                    'previous': previous_page,
-                                    'next': next_page})
+            return jsonify(item_list)
+
+        for item in bucketlist_items.items:
+            result = {
+                "id": item.item_id,
+                "name": item.name,
+                "description": item.description,
+                "completed": item.completed,
+                "date_created": item.date_created,
+                "date_modified": item.date_modified,
+                "bucketlist": item.bucketlist.name,
+                "owner": item.bucketlist.creator.username.decode('utf-8')
+            }
+            item_list.append(result)
+        next_page = 'None'
+        previous_page = 'None'
+        if bucketlist_items.has_next:
+            next_page = '{}api/v1/bucketlists/{}/items?limit={}&page={}'.format(
+                str(request.url_root),
+                str(bucketlist_id),
+                str(limit),
+                str(page_no + 1))
+        if bucketlist_items.has_prev:
+            previous_page = '{}api/v1/bucketlists/{}/items?limit={}&page={}'.format(
+                str(request.url_root),
+                str(bucketlist_id),
+                str(limit),
+                str(page_no - 1))
+        return jsonify({'bucketlist items': item_list,
+                        'total pages': bucketlist_items.pages,
+                        'previous': previous_page,
+                        'next': next_page})
 
     def post(self, bucketlist_id):
-        decode_token(request)
+        user_id = decode_token(request)
         data = json.loads(request.get_data(as_text=True))
         if not data:
             abort(400,
                   message="Kindly make sure you've passed all parameters")
-        if not data['name']:
+        if not data['item_name']:
             abort(400, message="Name cannot be empty")
-            name = data['name']
-            description = data['description']
-            completed = False
-            if 'completed' in data.keys():
-                completed = data['completed']
 
-            item = Items.query.filter_by(
+        name = data['item_name']
+        description = data['item_description']
+        completed = False
+        if 'completed' in data.keys():
+            completed = data['completed']
+
+        item = Items.query.filter_by(
+            name=name,
+            bucketlist_id=bucketlist_id).first()
+        if item:
+            abort(400, message="{} bucketlist item already exists".format(
+                item.name))
+        try:
+            new_bucketlist_item = Items(
                 name=name,
-                bucketlist_id=bucketlist_id).first()
-            if item:
-                abort(400, message="{} bucketlist item already exists".format(
-                    item.name))
-            try:
-                new_bucketlist_item = Items(
-                    name=name,
-                    description=description,
-                    completed=completed,
-                    bucketlist_id=bucketlist_id
-                )
-                db.session.add(new_bucketlist_item)
-                db.session.commit()
-                return jsonify({
-                    'message': "{} bucketlist was succefully created".format(
-                        name)})
-            except Exception:
-                abort(500,
-                      message="An error occured, Bucket list creation failed")
+                description=description,
+                completed=completed,
+                bucketlist_id=bucketlist_id
+            )
+            db.session.add(new_bucketlist_item)
+            db.session.commit()
+            return jsonify({
+                'message': "{} bucketlist was succefully created".format(
+                    name)})
+        except Exception:
+            abort(500,
+                message="An error occured, Bucket list creation failed")
 
 
 class OneBucketlistItem(Resource):
     def get(self, bucketlist_id, item_id):
         result = {}
-        decode_token(request)
+        user_id = decode_token(request)
         if bucketlist_id is None:
             abort(400, message="Provide bucketlist ID to proceed")
         if item_id is None:
@@ -142,13 +157,15 @@ class OneBucketlistItem(Resource):
             single_item.item_id: {
                 "name": single_item.name,
                 "description": single_item.description,
-                "completed": single_item.completed,
-                "bucketlist": single_item.bucketlist.name
+                "completed": bool(single_item.completed),
+                "id": single_item.item_id,
+                "date created": single_item.date_created,
+                "date modified": single_item.date_modified
             }})
         return jsonify(result)
 
     def put(self, bucketlist_id, item_id):
-        decode_token(request)
+        user_id = decode_token(request)
         if bucketlist_id is None:
             abort(400, message="Missing bucketlist Id")
         if item_id is None:
@@ -163,27 +180,33 @@ class OneBucketlistItem(Resource):
 
         if not single_item:
             abort(400,
-                  message="No bucketlist matching the id{}".format(
+                  message="No bucketlist matching the id {}".format(
                       bucketlist_id))
 
         try:
-            if 'name' in data.keys():
-                single_item.name = data['name']
-            if 'description' in data.keys():
-                single_item.description = data['description']
+            if 'new_item_name' in data.keys():
+                single_item.name = data['new_item_name']
+            if 'new_item_description' in data.keys():
+                single_item.description = data['new_item_description']
             if 'completed' in data.keys():
-                single_item.description = data['completed']
+                if data['completed'].upper() == "TRUE":
+                    single_item.completed = True
+                elif data['completed'].upper() == "FALSE":
+                    single_item.completed = False
+                else:
+                    abort(400, message='Invalid complete parameters. use True or False.')
+                
             single_item.date_modified = datetime.utcnow()
             db.session.add(single_item)
-            db.commit()
+            db.session.commit()
             return jsonify({
-                'message': "{} item was added successfully".format(
+                'message': "{} item was updated successfully".format(
                     single_item.name)})
         except Exception:
-            abort(500, message="An error occured, Item was not created")
+            abort(500, message="An error occured, Item was not updated")
 
     def delete(self, bucketlist_id, item_id):
-        decode_token(request)
+        user_id = decode_token(request)
         if bucketlist_id is None:
             abort(400, message="Missing bucketlist ID")
         if item_id is None:
@@ -192,11 +215,11 @@ class OneBucketlistItem(Resource):
             bucketlist_id=bucketlist_id,
             item_id=item_id).first()
         if not single_item:
-            abort(400, message="No item matching the id{}".format(
+            abort(400, message="No item matching the id {}".format(
                 item_id))
         try:
             db.session.delete(single_item)
-            db.commit()
+            db.session.commit()
             return jsonify({
                 'message': "{} Item deleted successfuly".format(
                     single_item.name)})
