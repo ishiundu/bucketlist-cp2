7be529689c7182280794a33fe9c5461c26e6e4f5
izlyr <fuzzkillofficial@gmail.com>
2017-01-31 20:26:50 +0300

    Make failing tests pass

--- app/bucketlists/test_bucketlist.py
+++ app/bucketlists/test_bucketlist.py
@@ -12,12 +12,14 @@ class BucketlistTest(GlobalTestCase):
     def setUp(self):
         db.drop_all()
         db.create_all()
+
         self.user = Users(
             username='johndoe',
             email='johndoe123@andela.com',
-            password='john123')
+            password='johndoe123')
         db.session.add(self.user)
         db.session.commit()
+
         response = self.client.post(
             url_for('login'),
             data=json.dumps({
@@ -25,17 +27,18 @@ class BucketlistTest(GlobalTestCase):
                 'password': 'johndoe123'}),
             content_type='application/json')
         data = json.loads(response.get_data(as_text=True))
+
         self.token = {'Authorization': data['token']}
-        self.logged_in_user = Users.query.filter_by(username='Loice').first()
+        self.logged_in_user = Users.query.filter_by(username='johndoe').first()
 
     def test_can_create_bucketlist(self):
         response = self.client.post(
             url_for('bucketlists'),
             data=json.dumps({
-                'name': 'test_bucketlist',
-                'description': 'Test bucketlist',
+                'bucket_name': 'test_bucketlist',
+                'bucket_description': 'Test bucketlist',
                 'date_created': str(datetime.datetime.now()),
-                        'creator_id': self.logged_in_user.user_id
+                'creator_id': self.logged_in_user.user_id
             }),
             content_type='application/json',
             headers=self.token)
@@ -47,10 +50,10 @@ class BucketlistTest(GlobalTestCase):
         self.client.post(
             url_for('bucketlists'),
             data=json.dumps({
-                'name': 'test_bucketlist',
-                'description': 'Test bucketlist',
+                'bucket_name': 'test_bucketlist',
+                'bucket_description': 'Test bucketlist',
                 'date_created': str(datetime.datetime.now()),
-                        'creator_id': self.logged_in_user.user_id
+                'creator_id': self.logged_in_user.user_id
             }),
             content_type='application/json',
             headers=self.token)
@@ -65,10 +68,10 @@ class BucketlistTest(GlobalTestCase):
         self.client.post(
             url_for('bucketlists'),
             data=json.dumps({
-                'name': 'test_bucketlist',
-                'description': 'Test bucketlist',
+                'bucket_name': 'test_bucketlist',
+                'bucket_description': 'Test bucketlist',
                 'date_created': str(datetime.datetime.now()),
-                        'creator_id': self.logged_in_user.user_id
+                'creator_id': self.logged_in_user.user_id
             }),
             content_type='application/json',
             headers=self.token)
@@ -88,56 +91,55 @@ class BucketlistTest(GlobalTestCase):
         self.client.post(
             url_for('bucketlists'),
             data=json.dumps({
-                'name': 'test_bucketlist',
-                'description': 'Test bucketlist',
-                'date_creation': str(datetime.datetime.now()),
-                        'creator_id': self.logged_in_user.user_id
+                'bucket_name': 'test_bucketlist',
+                'bucket_description': 'Test bucketlist',
+                'date_created': str(datetime.datetime.now()),
+                'creator_id': self.logged_in_user.user_id
             }),
             content_type='application/json',
             headers=self.token)
         response = self.client.get(
-            'api/v1/bucketlists?q=bucketlist',
+            '/bucketlists?q=bucketlist',
             headers=self.token)
         self.assert_200(response)
         data = json.loads(response.get_data(as_text=True))
         self.assertIsNotNone(data)
 
         response = self.client.get(
-            'api/v1/bucketlists?q=none',
+            '/bucketlists?q=none',
             headers=self.token)
         self.assert_status(response, 400)
         result = json.loads(response.get_data(as_text=True))
         self.assertIsNotNone(result)
-        self.assertIn("Sorry doesnot much any bucketlist item",
-                      result['message'])
+        self.assertIn("does not match any bucketlist names", result['message'])
 
     def test_can_edit_bucketlist(self):
         self.client.post(
             url_for('bucketlists'),
             data=json.dumps({
-                'name': 'test_bucketlist',
-                'description': 'Test my bucketlist',
-                'date_creation': str(datetime.datetime.now()),
-                        'creator_id': self.logged_in_user.user_id
+                'bucket_name': 'test_bucketlist',
+                'bucket_description': 'Test bucketlist',
+                'date_created': str(datetime.datetime.now()),
+                'creator_id': self.logged_in_user.user_id
             }),
             content_type='application/json',
             headers=self.token)
         response = self.client.put(
             url_for('one_bucketlist', bucketlist_id=1),
             data=json.dumps({
-                'name': 'Adventure',
-                'description': 'sky diving in Australia',
-                'date_creation': str(datetime.datetime.now()),
-                        'creator_id': self.logged_in_user.user_id
+                'bucket_name': 'life_bucketlist',
+                'bucket_description': 'Life bucketlist',
+                'date_created': str(datetime.datetime.now()),
+                'creator_id': self.logged_in_user.user_id
             }),
             headers=self.token)
         self.assert_200(response)
         data = json.loads(response.get_data(as_text=True))
         self.assertIsNotNone(data)
 
-    def tearDown(self):
-        db.session.close_all()
-        db.drop_all()
+    # def tearDown(self):
+    #     db.session.close_all()
+    #     db.drop_all()
 
 
 if __name__ == '__main__':
